2eaa064d235850daedf7479f1472213db0403740
Malachi Spencer <malachispencer@Malachis-Air.fritz.box>
2020-11-04 21:39:17 +0000

    Added comments to explain tests

--- spec/exercise_1_spec.rb
+++ spec/exercise_1_spec.rb
@@ -1,3 +1,14 @@
+=begin
+Makers. 04/11/20. Here I implement the tests to test the classes in isolation from each other using doubles.
+1) For the Note class, I created a simple double called formatter.
+2) I then created a Note instance and placed the formatter double in as the optional formatter parameter.
+3) Inside the test for the display method, inside the it block, I use a method stub to allow the formatter double to receive
+   the format method and return the formatted string with the actual title and body of the Note instance we created for the
+   test.
+4) When the display method is called on our Note instance, it uses our formatter double, calls format on it and our format
+   mimics what the actual format method would do, returning the formatted note.
+=end
+
 require 'exercise_1'
 
 describe Note do
@@ -7,7 +18,7 @@ describe Note do
   describe '#display' do
     it 'prints a formatted note' do
       allow(formatter).to receive(:format) { "Title: #{note.title}\n#{note.body}" }
-      expect(note.display).to eq("Title: #{note.title}\n#{note.body}")
+      expect(note.display).to eq("Title: hard day\nextracting classes was tough")
     end
   end
 end
