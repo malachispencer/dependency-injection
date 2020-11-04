a784a178e8eab5377853b54e92d194c993c78984
Malachi Spencer <malachispencer@Malachis-Air.fritz.box>
2020-11-04 21:28:09 +0000

    Added explanation of refactoring

--- lib/exercise_1.rb
+++ lib/exercise_1.rb
@@ -1,5 +1,15 @@
+=begin
+Makers. 04/11/20. Here I had to refactor the Note class to make it testable without having to actually use its dependency
+class - NoteFormatter - in the tests. The indication that I had done this successfully would be if I could test the output
+of display while the format method is deleted (or commented out).
+1) In order to refactor, I added a 3rd parameter to the initialize method of Note, formatter = NoteFormatter.new.
+2) Inside the intiailize, I then replaced @formatter = NoteFormatter.new with @formatter = formatter.
+3) What this now means is that while Note will ordinarily take in NoteFormatter instance by default, we can test the
+   functionality of Note with a double that mimics its behaviour.
+=end
+
 class Note
-  def initialize(title, body, formatter = Formatter.new)
+  def initialize(title, body, formatter = NoteFormatter.new)
     @title = title
     @body = body
     @formatter = formatter
