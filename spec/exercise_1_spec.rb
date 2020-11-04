46a68eadc0472b2c17025e493ec23547a0df03d7
Malachi Spencer <malachispencer@Malachis-Air.fritz.box>
2020-11-04 21:43:19 +0000

    Explained NoteFormatter test and double

--- spec/exercise_1_spec.rb
+++ spec/exercise_1_spec.rb
@@ -7,6 +7,10 @@ Makers. 04/11/20. Here I implement the tests to test the classes in isolation fr
    test.
 4) When the display method is called on our Note instance, it uses our formatter double, calls format on it and our format
    mimics what the actual format method would do, returning the formatted note.
+5) For the NoteFormatter class, when we call the format method we take in a Note instance, so we want to be able to test that
+   in an isolated fashion too.
+6) We create a Note double with our title and body properties, then simply pass this Note double into the format method
+   and it returns our formatted note. No need at all to use the Note class itself.
 =end
 
 require 'exercise_1'
@@ -28,7 +32,7 @@ describe NoteFormatter do
   
   describe '#format' do
     it 'prints a formatted note' do
-      expect(subject.format(note)).to eq("Title: #{note.title}\n#{note.body}")
+      expect(subject.format(note)).to eq("Title: hard day\nextracting classes was tough")
     end
   end
 end
