4a75ddf8a365425c37ee7fc75307ed18124fd258
Malachi Spencer <malachispencer@Malachis-Air.fritz.box>
2020-11-04 20:59:17 +0000

    Added explanation of refactoring

--- lib/exercise_2.rb
+++ lib/exercise_2.rb
@@ -1,16 +1,25 @@
+=begin
+Makers. 04/11/20. Here I had to amend the Diary class in order to make it testable in isolation - using a double - from its
+dependency class Entry. Here is how I did it.
+1) I deleted Entry.new(title, body) from the add method.
+2) I placed a parameter in the initialize method, entry_class, which is by default the Entry class. But we can optionally
+   make it a double. This is what ultimately allows us to then use a double to test Diary in an isolated manner.
+3) Inside initialize, I set the @entry_class instance variable equal to the entry_class parameter.
+4) In add, we create a new instance of the Entry class, or this can be stubbed in our test.
+=end
+
 class Diary
-  def initialize
+  def initialize(entry_class = Entry)
     @entries = []
+    @entry_class = entry_class
   end
 
-  def add(title, body)
-    @entries << Entry.new(title, body)
+  def add
+    @entries << @entry_class.new
   end
 
   def index
-    titles = @entries.map do |entry|
-      entry.title
-    end
+    titles = @entries.map { |entry| entry.title }
     titles.join("\n")
   end
 end
