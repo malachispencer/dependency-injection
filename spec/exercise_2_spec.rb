c5ddc91a6b53fb37b26bf678cf84ea8267761ac3
Malachi Spencer <malachispencer@Malachis-Air.fritz.box>
2020-11-04 21:47:06 +0000

    Explained possible allow alternative

--- spec/exercise_2_spec.rb
+++ spec/exercise_2_spec.rb
@@ -3,12 +3,14 @@ Makers. 04/11/20. Here I had to create a test for the Diary class, which is depe
 use the actual Entry class in the test.
 1) First I created a double for an Entry instance. Like an Entry instance, it has a title and a body as its properties.
 2) I then created a double for the Entry class itself. The double possesses a new property, which can also be considered a
-   new method, and this returns the entry double we created on the previous line.
-3) We set our Diary class to take in the Entry class or an Entry double, so we pass in our entry_class double to the Diary
+   new method, and we set this to return the entry double we created on the previous line.
+3) I also could have made a very basic entry_class double with no property/method. Then inside the it block I could have used
+   allow(entry_class).to receive(:new) { entry }.
+4) We set our Diary class to take in the Entry class or an Entry double, so we pass in our entry_class double to the Diary
    instance we will use for our test.
-4) In our it block, we call the add method on the Diary instance. Which will call the new method on the entry_class double,
+5) In our it block, we call the add method on the Diary instance. Which will call the new method on the entry_class double,
    which will return our entry double and push it into the @entries array.
-5) We then call the index instance method, which prints all the entry titles, and since we only added 1 title, it should
+6) We then call the index instance method, which prints all the entry titles, and since we only added 1 title, it should
    return the title inside our entry double.
 =end
 
