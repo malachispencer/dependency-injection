03c61c52b90d203c1cdf568cf10b525bdec5094e
Malachi Spencer <malachispencer@Malachis-Air.fritz.box>
2020-11-05 13:14:48 +0000

    Finished comments for exercise 3

--- lib/exercise_3.rb
+++ lib/exercise_3.rb
@@ -5,6 +5,10 @@ doubles and stubs.
 2) I took EmailClient.new out of the run method and made it a parameter of the run method.
 3) Nothing inside the Message class needed to change, because we are not testing the actual Message class in either of our
    tests or using its send method.
+4) Prior to my coaching call with Tatsiana, I had added in initalize methods for EmailClient and SayHello, but not only was
+   this unnecessary, she said when refactoring for exercises we generally don't need to add in methods.
+5) Dependency injections can be done directly into methods, and this is preferable if only one method in the class needs
+   that dependency. If more than one class needs a dependency, it can be injected in the initialize method of that class.
 =end
 
 class EmailClient
--- spec/exercise_3_spec.rb
+++ spec/exercise_3_spec.rb
@@ -1,19 +1,31 @@
 =begin
-Makers. 05/11/20.
+Makers. 05/11/20. Here I wrote the tests for the SayHello and EmailClient classes, testing them in isolation.
+1) For SayHello, we first create a double for an EmailClient instance, which we can pass in to the run method to act as a
+   fake EmailClient instance.
+2) In the actual method, the email_client instance has a message method called on it, so we create a method stub for that
+   message method.
+3) The message method, when passed no parameters, returns an instance of the Message class, so we create a simple double
+   that represents an instance of the Message class and make our message method stub return that double.
+4) The message instance then has a send method called on it, so we need a stub for the send method, we allow the message
+   instance to receive the send method and return a fake message of an email being sent.
+5) In our expectation we check that our tests produces the expected result.
+6) The EmailClient class is dependent on the Message class and the message method of the EmailClient class simply returns
+   what is passed into it. So we create a message_instance double and simply check whether the message method returns that
+   double.
+7) From the looks of our tests, we mimic classes and class instances using doubles whereas we mimic methods using stubs.
 =end
 
 require 'exercise_3'
 
 describe SayHello do
   describe '#run' do
-    let(:say_hello) { SayHello.new }
     let(:email_client) { double :email_client }
     let(:message_instance) { double :message_instance }
 
     it 'sends HELLO! to friend@example.com' do
       allow(email_client).to receive(:message) { message_instance }
       allow(message_instance).to receive(:send) { "sent HELLO! to friend@example.com\n" }
-      expect(say_hello.run(email_client)).to eq("sent HELLO! to friend@example.com\n")
+      expect(subject.run(email_client)).to eq("sent HELLO! to friend@example.com\n")
     end
   end
 end
