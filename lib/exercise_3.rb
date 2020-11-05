0e00fca56ea8ba0b0eb90fbc549ef0ba558c1d5b
Malachi Spencer <malachispencer@Malachis-Air.fritz.box>
2020-11-05 12:58:33 +0000

    Added explanation for exercise 3

--- lib/exercise_3.rb
+++ lib/exercise_3.rb
@@ -1,5 +1,10 @@
 =begin
-Makers. 05/11/20.
+Makers. 05/11/20. Here I had to refactor the SayHello and EmailClient classes to make them testable in isolation using
+doubles and stubs.
+1) I took Message.new out of the message method and made it parameter of the message method instead.
+2) I took EmailClient.new out of the run method and made it a parameter of the run method.
+3) Nothing inside the Message class needed to change, because we are not testing the actual Message class in either of our
+   tests or using its send method.
 =end
 
 class EmailClient
